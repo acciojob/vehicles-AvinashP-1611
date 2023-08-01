52202150d18f638312478710c3aaa0c97460fe8b
Avinash Prajapati <avinashprajapati1611r@gmail.com>
2023-08-01 14:21:37 +0530

    changes

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,25 +4,5 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
-class Boat implements WaterVehicle{
-    String name;
-    int capacity;
-    public Boat(String name,int capacity){
-        this.capacity=capacity;
-        this.name=name;
-    }
 
-    @Override
-    public String getVehicleName() {
-        return null;
-    }
 
-    @Override
-    public int getVehicleCapacity() {
-        return this.capacity;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-}
